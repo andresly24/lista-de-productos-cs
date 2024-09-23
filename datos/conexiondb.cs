e64a882d299da0893ba1f2e4687418334a2063a6
andresly24 <andresliye02@gmail.com>
2024-09-23 14:59:16 -0500

    ver 1.0

---
+++ datos/conexiondb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datos
+{
+    public class Conexiondb
+    {
+
+        string conexionpathURL = "Server=DESKTOP-MFBH1RN\\\\SQLEXPRESS;Database=objetos;Trusted_Connection=True;\" + \"TrustServerCertificate=true";
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-MFBH1RN\\SQLEXPRESS;Database=objetos;Trusted_Connection=True;" + "TrustServerCertificate=true");
+        
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed) 
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+
+        public string ConexionPathURL
+        {
+            get { return conexionpathURL; }
+        }
+    }
+}
---
+++ datos/d_consultar.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace datos
+{
+    public class D_consultar
+    {
+        private Conexiondb conexion = new Conexiondb();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            //comando.CommandText = "Select * from objetos";
+            // SP
+            
+            comando.CommandText = "MostrarItems";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+        }
+
+        public void Insertar(string item, double precio)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsertarItems";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@item",item);
+            comando.Parameters.AddWithValue("@precio", precio);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+        }
+
+        public void Editar(int id,string item, double precio)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditarItems";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+            comando.Parameters.AddWithValue("@item", item);
+            comando.Parameters.AddWithValue("@precio", precio);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+        }
+
+        public void Eliminar(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarItems";
+            comando.CommandType= CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@iditem", id);
+            comando.ExecuteNonQuery();
+            //comando.Parameters.Clear();
+        }
+
+        public void Descargar()
+        {
+            string conexionpath = conexion.ConexionPathURL;
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", "excel.xlsx");
+            var query = "SELECT * FROM objetos";
+
+            try
+            {
+                using (SqlConnection conexion2 = new SqlConnection(conexionpath))
+                {
+                    conexion2.Open();
+                    SqlCommand cmd = new SqlCommand(query, conexion2);
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable datatabla = new DataTable();
+                    adapter.Fill(datatabla);
+
+                    using (var workbook = new XLWorkbook())
+                    {
+                        var worksheet = workbook.Worksheets.Add("Info");
+                        worksheet.Cell(1, 1).InsertTable(datatabla);
+                        workbook.SaveAs(filePath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+    }
+}
